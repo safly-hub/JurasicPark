690645ae9f9301371ccf06a43735ba2dc0683de3
LUCAS SAFLY <saflycorp@gmail.com>
2021-01-14 10:25:33 -0500

    CSV HELPER AND FILE / STREAM WRITERS

--- Program.cs
+++ Program.cs
@@ -1,16 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
 
 namespace JurasicPark
 {
     class Dinosaurs
     {
+        var new Herbivorous List<string> Herbivors();
+        var new Carnivorous List<string> Carnivors();
 
+        var new Dinosaurs WhenAqcquired = new List<DateTime>();
         public string Name { get; set }
         public string DietType { get; set }
         public int WhenAcquired { get; set }
 
         public string WeightinLBs { get; set; }
         public int EnclosureNumber { get; set; }
+    }
+    class UserInput
+    {
+
+        //add
+        //remove
+        //quit
+
+
     }
     class Program
     {
@@ -34,6 +50,7 @@ namespace JurasicPark
     }
     static void Main(string[] args)
     {
+
         //////Console.Write(GreetingandMenu);
         while (userHasChosenToQuit == false)
         {
@@ -60,5 +77,14 @@ namespace JurasicPark
 
 
         }
+        //when user has quit 
+        //Time to write the file
+        //make filewriting stream
+        var fileWriter = new StreamWriter("numbers.csv");
+        var csvWriter = new CsvWriter(fileWriter, CultureInfo.InvariantCulture);
+        //numbers could be Dinos and / or features
+        //PROBABLY RESPONSES TO ADD/CHANGE FEATURES OR RESPOND TO MENU 
+        csvWriter.WriteRecords(numbers);
+        fileWriter.Close();
     }
 }
